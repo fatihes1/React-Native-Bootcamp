afee34a033488a1185d4dbd9447692b9406b6bc7
fatihes1 <18360859021@ogrenci.btu.edu.tr>
2022-07-13 07:58:33 +0300

    Component Structure & first style prop.

--- myFirstProject/App.js
+++ myFirstProject/App.js
@@ -2,17 +2,25 @@
 // Ekranda görünen her şey en temeline component'tir.
 
 import React from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import {SafeAreaView, View,  Text, StyleSheet} from 'react-native';
 
 // View, web'deki <div> | Text ise web'deki <p> taglerine benzer !
 
 const App = () => {
   return (
-    <View>
+    <SafeAreaView>
+    <View style={{backgroundColor: "blue"}}>
+      <Text>
+        Hello World !
+      </Text>
       <Text >
         Hello World !
       </Text>
     </View>
+    <Text >
+        Hello World !
+      </Text>
+    </SafeAreaView>
   );
 }
 
