fa9dfbba371577b2a45481e4cd76be2fab4d734c
fatihes1 <18360859021@ogrenci.btu.edu.tr>
2022-07-15 13:44:13 +0300

    styling & align & direction in styling

--- myFirstProject/App.js
+++ myFirstProject/App.js
@@ -14,8 +14,9 @@ const App = () => {
 
   };
   return (
-    <SafeAreaView>
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      {/*  FIRST PART */}
+    {/* <View style={styles.upper_view_container}>
       <Text >
         Hello World !
       </Text>
@@ -23,27 +24,83 @@ const App = () => {
         Hello World !
       </Text>
     </View>
+      <View style={styles.middle_container}>
       <Text >
         Hello World !!
-      </Text>
+      </Text> */}
       {/* sadece disabled yazarsak da olur. boolen değeri true olacak ise süslü parantez ile true yazmaya gerek yok. */}
-      <Button disabled={false} title="Click me!" color={"indigo"} onPress={() => {sayHello("Fatih")}}/>
-      
+      {/* <Button disabled={false} title="Click me!" color={"indigo"} onPress={() => {sayHello("Fatih")}}/>
+      </View>
+      <View style={styles.bottom_view_container}>
+      <Text >
+        Merhaba Dünya !
+      </Text>
+    </View> */}
+   {/*  ------------------------------------------ */}
+
+     {/*  SECOND PART */}
+      <View style={styles.box_1} />
+      <View style={styles.box_2} />
+      <View style={styles.box_3} />
+      <View style={styles.box_4} />
     </SafeAreaView>
   );
 }
 
  const styles = StyleSheet.create({
   container : {
-    backgroundColor : 'red',
-    margin: 10,
-    padding: 10,
-    borderRadius: 5,
+    flex: 1,
+    flexDirection : 'column',
+    backgroundColor: 'yellow',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
   },
-  buttonStyle : {
-    width: 80,
+  // FIRST PART CSS
+  // upper_view_container : {
+  //   flex : 2,
+  //   backgroundColor : 'red',
+  //   margin: 10,
+  //   padding: 10,
+  //   borderRadius: 5,
+  // },
+  // middle_container : {
+  //   flex: 1,
+  // },
+  // bottom_view_container : {
+  //   flex : 2,
+  //   backgroundColor : 'blue',
+  //   color: 'white',
+  //   margin: 10,
+  //   padding: 10,
+  //   borderRadius: 5,
+  // },
+  // buttonStyle : {
+  //   width: 80,
     
-  }
+  // }
+
+  // -------------------------------------
+  // SECOND PART
+  box_1 : {
+    width: 75,
+    height: 75,
+    backgroundColor: 'red'
+  }, 
+  box_2 : {
+    width: 75,
+    height: 75,
+    backgroundColor: 'blue'
+  },
+  box_3 : {
+    width: 75,
+    height: 75,
+    backgroundColor: 'green'
+  },
+  box_4 : {
+    width: 75,
+    height: 75,
+    backgroundColor: 'orange'
+  },
  })
 
 
