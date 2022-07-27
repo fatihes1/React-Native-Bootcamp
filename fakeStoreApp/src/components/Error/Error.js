b208f0fd7b98506017f9fddda4d5ce5599a8a112
fatihes1 <18360859021@ogrenci.btu.edu.tr>
2022-07-27 11:21:42 +0300

    Error & Loading animation | LottieView

---
+++ fakeStoreApp/src/components/Error/Error.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LottieView from "lottie-react-native";
+
+
+const Error = () => {
+    return <LottieView source={require('../../assets/error.json')} autoPlay />
+}
+
+
+export default Error
--- fakeStoreApp/src/pages/Products/Products.js
+++ fakeStoreApp/src/pages/Products/Products.js
@@ -6,22 +6,24 @@ import axios from 'axios';
 import styles from './Products.style';
 import ProductCard from '../../components/ProductCard';
 import useFetch from '../../hooks/useFetch/useFetch';
+import Loading from '../../components/Loading/Loading';
+import Error from '../../components/Error/Error';
 
 
 const Products = () => {
     // CUSTOM HOOK USAGE
-    const {error, loading, data} = useFetch(Config.API_URL);
+    const {error, loading, data} = useFetch(Config.API_URL + "asddsa");
 
     const renderProduct = ({item}) => {
        return <ProductCard product={item} />
     }
 
     if(error){
-        return <Text>{error}</Text>
+        return <Error />
     }
 
     if(loading){
-        return <ActivityIndicator size="large" />
+        return <Loading />
     }
     
     return(
